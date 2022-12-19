4e68da06acc7648e38872330e1a3a202c5ede00e
atralex <atralexrg@gmail.com>
2022-12-20 00:21:36 +0100

    Correccion comportamiento formulario

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const formu = document.getElementById("formulario");
 const template = document.getElementById("oculto").content;
 const card = document.getElementById("card");
 const cacho = document.createDocumentFragment();
+var valida = true;
 
 
 var requestOptions = {
@@ -15,17 +16,50 @@ var requestOptions = {
 };
 formu.addEventListener("submit", e => {
   e.preventDefault();
+  console.log(color.value);
+  console.log(type.value);
+  if((color.value == "")&&(type.value == "")){
+    var name = n.value;
+    console.log(name);
+      fetch("https://digimoncard.io/api-public/search.php?n=" + name, requestOptions)
+        .then(response => response.json())
+        .then(result => {
+          console.log(result);
+          pintarCard(result);
+        })
+        .catch(error => console.log('error', error));
+  } else if((color.value == "")&&(type.value != "")){
+    var name = n.value;
+    console.log(name);
+      fetch("https://digimoncard.io/api-public/search.php?n=" + name + "&type=" + type.value, requestOptions)
+        .then(response => response.json())
+        .then(result => {
+          console.log(result);
+          pintarCard(result);
+        })
+        .catch(error => console.log('error', error));
+  } else if((color.value != "")&&(type.value == "")){
+    var name = n.value;
+    console.log(name);
+      fetch("https://digimoncard.io/api-public/search.php?n=" + name + "&color=" + color.value, requestOptions)
+        .then(response => response.json())
+        .then(result => {
+          console.log(result);
+          pintarCard(result);
+        })
+        .catch(error => console.log('error', error));
+  }
   var name = n.value;
   console.log(name);
-  fetch("https://digimoncard.io/api-public/search.php?n=" + name + "&color=" + color.value + "&type=" + type.value, requestOptions)
-    .then(response => response.json())
-    .then(result => {
-      console.log(result);
-      pintarCard(result);
-    })
-    .catch(error => console.log('error', error));
-
-})
+    fetch("https://digimoncard.io/api-public/search.php?n=" + name + "&color=" + color.value + "&type=" + type.value, requestOptions)
+      .then(response => response.json())
+      .then(result => {
+        console.log(result);
+        pintarCard(result);
+      })
+      .catch(error => console.log('error', error));
+  }
+)
 pintarCard = result => {
   card.innerHTML = " ";
   result.forEach(el => {
